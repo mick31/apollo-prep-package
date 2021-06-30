429ba0f155184194e72ea50d6606db74caca6df3
mick <micbennett@expedia.com>
2021-06-30 09:48:18 -0700

    created Package file created workspace

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "ApolloPrepPackage",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(name: "ApolloPrepPackage", targets: ["ApolloPrepPackage"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ApolloPrepPackage",
+            dependencies: [],
+            path: "ApolloPrepPackage/Sources")
+    ]
+)
